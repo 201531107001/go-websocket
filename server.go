dbee202580605602e4d23cb61116e9cd031505bc
gqm <2571197208@qq.com>
2019-07-10 23:56:28 +0800

    '完善交互'

--- server.go
+++ server.go
@@ -19,23 +19,25 @@ func main() {
 	http.HandleFunc("/ws", func(writer http.ResponseWriter, request *http.Request) {
 		var (
 			conn *websocket.Conn
-			err  error
 			date []byte
+			err  error
 		)
 		if conn, err = upgrader.Upgrade(writer, request, nil); err != nil {
 			return
 		}
-
-		for true  {
-			if _,date,err =conn.ReadMessage();err !=nil {
+        conn.WriteMessage(websocket.TextMessage, []byte("连接成功"))
+		for true {
+			if _, date, err = conn.ReadMessage(); err != nil {
+                conn.WriteMessage(websocket.TextMessage, []byte("断开连接"))
 				conn.Close()
-				fmt.Println("断开连接",err)
+				fmt.Println("断开连接", err)
 				return
 			}
 			fmt.Println(string(date))
-			conn.WriteMessage(websocket.TextMessage,[]byte("Hello Client"))
+			conn.WriteMessage(websocket.TextMessage, date)
 		}
 	})
 
 	http.ListenAndServe("0.0.0.0:8080", nil)
 }
+
